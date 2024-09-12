115818eb76f41bddf537fa44f4f5d66e6e478dd1
sujit <partesujit1@gmail.com>
2024-09-12 20:03:35 +0000

    solution

---
+++ src/main/java/com/example/helloworld/HelloworldController.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloworldController{
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
